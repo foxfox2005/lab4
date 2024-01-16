52e2918fbdb5dd6e8a2c1224d8031872bdd2119b
foxfox2005 <83116608+foxfox2005@users.noreply.github.com>
2024-01-16 21:09:37 +0700

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         sanPham sanpham1 = new sanPham();
-        System.out.printf("nhap sp: ");
+        sanPham sanpham2 = new sanPham();
+
+        // sp1
+        System.out.printf("nhap san pham 1: ");
         Scanner sc = new Scanner(System.in);
-        String sp = sc.nextLine();
-        System.out.printf("gia");
-        double gia = sc.nextDouble();
-        System.out.printf("giam gia sp");
-        double giamGia = sc.nextDouble();
+        String sp1 = sc.nextLine();
+        System.out.printf("gia 1: ");
+        double gia1 = sc.nextDouble();
+        System.out.printf("giam gia 1: ");
+        double giamGia1 = sc.nextDouble();
 
-        sanpham1.tenSp = sp;
-        sanpham1.giaSp = gia;
-        sanpham1.giamgia = giamGia;
+        // sp2
+        System.out.printf("\nnhap sp 2: ");
+        String sp2 = sc.nextLine();
+        System.out.printf("\ngia 2: ");
+        double gia2 = sc.nextDouble();
+        System.out.printf("giam gia 2: ");
+        double giamGia2 = sc.nextDouble();
 
+        sanpham1.tenSp = sp1;
+        sanpham1.giaSp = gia1;
+        sanpham1.giamgia = giamGia1;
         sanpham1.xuat();
+
+        sanpham2.tenSp = sp2;
+        sanpham2.giaSp = gia2;
+        sanpham2.giamgia = giamGia2;
+        sanpham2.xuat();
     }
 }
