8d8181ae93ecb87e8ceda1c0a4a574a82ef42d73
foxfox2005 <83116608+foxfox2005@users.noreply.github.com>
2024-01-17 10:13:08 +0700

    final 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
         sanPham sanpham1 = new sanPham();
         sanPham sanpham2 = new sanPham();
 
         // sp1
         System.out.printf("nhap san pham 1: ");
-        Scanner sc = new Scanner(System.in);
         String sp1 = sc.nextLine();
         System.out.printf("gia 1: ");
         double gia1 = sc.nextDouble();
@@ -17,18 +18,19 @@ public class Main {
         double giamGia1 = sc.nextDouble();
 
         // sp2
-        System.out.printf("\nnhap sp 2: ");
-        String sp2 = sc.nextLine();
-        System.out.printf("\ngia 2: ");
+        System.out.printf("nhap san pham 2: ");
+        String sp2 = sc.next(); // Correctly read product name as string
+        System.out.printf("gia 2: ");
         double gia2 = sc.nextDouble();
         System.out.printf("giam gia 2: ");
         double giamGia2 = sc.nextDouble();
 
+        System.out.println("san pham 1");
         sanpham1.tenSp = sp1;
         sanpham1.giaSp = gia1;
         sanpham1.giamgia = giamGia1;
         sanpham1.xuat();
-
+        System.out.println("\nsang pham 2");
         sanpham2.tenSp = sp2;
         sanpham2.giaSp = gia2;
         sanpham2.giamgia = giamGia2;
